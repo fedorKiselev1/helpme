368766bd7f8d058ebdf8817cc4118172de81d18a
Milk <146938471+fedorKiselev1@users.noreply.github.com>
2026-01-13 15:57:00 +0300

    docker prisma

--- cook/cook/src/auth/prisma/prisma.service.ts
+++ cook/cook/src/auth/prisma/prisma.service.ts
@@ -1,2 +1,16 @@
 import { Injectable, OnModuleDestroy, OnModuleInit } from "@nestjs/common";
-import { PrismaClient } from ;
+import { PrismaClient } from './../../../prisma/generated';
+
+@Injectable()
+export class PrismaService
+    extends PrismaClient    
+    implements OnModuleInit, OnModuleDestroy
+{
+    public async onModuleInit(): Promise<void> {
+        await this.$connect();
+    }
+
+    public async onModuleDestroy(): Promise<void> {
+        await this.$disconnect();
+    }
+}
